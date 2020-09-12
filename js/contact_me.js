8182c0e42fcfd3073f31de2dd7ac09486f58e23e
Jeff <westerly.lzh@gmail.com>
2020-09-12 18:12:56 +0800

    change icons and enable email

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,34 +19,39 @@ $(function() {
             }
             //refrence https://github.com/zbeekman/zbeekman.github.io/blob/master/js/contact_me.js
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "https://formspree.io/xknpwwdk",
+                method: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
                 },
+		dataType: "json",
                 cache: false,
-                success: function() {
+                success: function(data) {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>")
+		    $('#success > .alert-success')
+                        .append(firstName)
+		    $('#success > .alert-success')
+                        .append(", 您提交的信息已经成功，多谢您的支持.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(err) {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", 邮件服务器暂时没有相应，请稍后再次尝试!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
